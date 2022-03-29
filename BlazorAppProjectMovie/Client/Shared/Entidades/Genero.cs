58d33c76e9a0956f3c0d5941c786adb60bd175bb
Trevolt11 <rrobertccaraballo@gmail.com>
2022-03-29 16:11:17 -0400

    Se agregaron nuevos componentes y la funcion de crear nuevos generos de pelicula

---
+++ BlazorAppProjectMovie/Client/Shared/Entidades/Genero.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorAppProjectMovie.Client.Shared.Entidades
+{
+    public class Genero
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El campo {0} es requiro para proceder")]
+        public string Name { get; set; }
+    }
+}
