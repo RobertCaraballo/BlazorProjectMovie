fd676a9016ad2be53d1e7b4760416b6ddead9771
Trevolt11 <rrobertccaraballo@gmail.com>
2022-04-01 16:42:42 -0400

    Ajustes en startup, errores en Program.cs

--- BlazorAppProjectMovie/Client/Program.cs
+++ BlazorAppProjectMovie/Client/Program.cs
@@ -3,7 +3,10 @@ using BlazorAppProjectMovie.Client.Repositorios;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Tewr.Blazor.FileReader;
-
+using Microsoft.Extensions.DependencyInjection;
+using System.Configuration;
+using FluentAssertions.Common;
+using Fluent.Infrastructure.FluentModel;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -17,10 +20,24 @@ await builder.Build().RunAsync();
 
 static void ConfigureSErvices(IServiceCollection services)
 {
+    services.AddControllers();
+
+    services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer("name=ConnectionStrings:DefaultConnection"));
 
     services.AddSingleton<ServiciosSingleton>();
     services.AddTransient<ServiciosTransistorio>();
     services.AddSingleton<IRepositorio, Repositorio>();
     services.AddFileReaderService(options => options.InitializeOnFirstCall = true);
 
+}
+
+
+public class MyController
+{
+    private readonly ApplicationDbContext _context;
+
+    public MyController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
 }
